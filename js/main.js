6d299e69cd7fd9944a73d265408184a76cf6ff6e
floris west <floris.westendorp@sintlucasedu.nl>
2024-02-20 12:38:40 +0100

    Changes
    Fixed first move
    Fixed Out Of Bounds
    Fixed offset calculation
    Added inventory

--- js/main.js
+++ js/main.js
@@ -1,39 +1,94 @@
-document.getElementById("mainTitle").innerText = "Point and click adventure game";
+document.getElementById("mainTitle").innerText = "Point and Click adventure game";
 
 //Game window reference
 const gameWindow = document.getElementById("gameWindow");
 
-//Game inventory
+//Inventory
+let inventory = [];
+console.log(inventory);
 const inventoryList = document.getElementById("inventoryList");
-
-//Main character
+//Main Character
 const mainCharacter = document.getElementById("hero");
 const offsetCharacter = 16;
+const tree1 = document.getElementById("squareTree");
 
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    //TODO: Calc offset based on character size
-    //FIX: Character doesn't animate on first move
-    //FIX: Out of bounds when clicking on itself
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+    //TODO: calc offset based on character size
+
+    if (e.target.id !== "heroImage") {
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
+    }
 
     switch (e.target.id) {
-        case "squareTree":
-            squareTree.style.opacity = 0.5;
         case "key":
-            console.log("You\'ve found a key!");
-            document.getElementById("key").remove();
-            const keyElement = document.createElement("li");
-            keyElement.id = "inv-key";
-            keyElement.innerText = "Key"
-            inventoryList.appendChild(keyElement)
+            getItem("Rusty key", "rustyKey");
             break;
+        case "well":
+            getItem("Coin", "coin");
+            break;
+        case "doorWizardHut":
+            if (checkItem("Rusty key")) {
+                console.log("I opened the door. Yeah!");
+            } else if (checkItem("Coin")) {
+                removeItem("Coin", "coin")
+                console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
+            } else {
+                console.log("Fuck this door is locked and I don't have a key. boohoo :(");
+            }
+            break;
+
         default:
-            squareTree.style.opacity = 1;
+            break;
+
     }
+    /**
+     * Checks if the value exists within the array
+     * If not then it adds value to the array and use showItem function
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function getItem(itemName, itemId) {
+        if (!checkItem(itemName)) {
+            inventory.push(itemName);
+            showItem(itemName, itemId);
+        }
+        console.log(inventory);
+    }
+
+    function checkItem(itemName) {
+        return inventory.includes(itemName);
+    }
+
+    /**
+     * Needs a name for displaying item and a html id name
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function showItem(itemName, itemId) {
+        console.log('You\'ve found a ' + itemName + '!');
+        const keyElement = document.createElement("li");
+        keyElement.id = itemId;
+        keyElement.innerText = itemName;
+        inventoryList.appendChild(keyElement);
+    }
+
+    /**
+     * Removes item from array and the element within the HTML
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function removeItem(itemName, itemId) {
+        //remove item in Array
+        inventory = inventory.filter(function (newInventory) {
+            return newInventory !== itemName;
+        });
+        document.getElementById(itemId).remove();
+    }
+
 
 }
